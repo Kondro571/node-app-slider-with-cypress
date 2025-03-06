853e72dfb619c506b47d1f9d437bcb3e891f2542
Kondro571 <133992948+Kondro571@users.noreply.github.com>
2025-03-06 08:42:20 +0100

    Update slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -17,74 +17,28 @@ describe('Swiper Gallery Test', function () {
   });
 });
 
-describe('Gallery Navigation Tests', () => {
-    beforeEach(() => {
+describe('check all buttons', function()  {
+
+    it('Should navigate to the next and previous slide', function() {
         cy.visit('http://localhost:3000'); 
-    });
+        cy.get('.swiper-slide-active').should('contain', 'Rome')
 
-    it('Should navigate to the next and previous slide', () => {
-        cy.get('.swiper-slide-active').then(($firstSlide) => {
-            const firstSlideText = $firstSlide.text();
+        cy.get('.swiper-button-next').click();
 
-            cy.get('.swiper-button-next').click();
-            cy.get('.swiper-slide-active').should(($newSlide) => {
-                expect($newSlide.text()).not.to.eq(firstSlideText);
-            });
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'London');
+        cy.get('.swiper-button-next').click();
 
-            cy.get('.swiper-button-prev').click();
-            cy.get('.swiper-slide-active').should(($backToFirstSlide) => {
-                expect($backToFirstSlide.text()).to.eq(firstSlideText);
-            });
-        });
-    });
-});
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'Paris')
+        cy.get('.swiper-button-next').click();
+        
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'Rome')
+        cy.get('.swiper-button-prev').click();
 
-describe('Gallery Slide Content Tests', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000');
-    });
+        cy.wait(2000);
+        cy.get('.swiper-slide-active').should('contain', 'Paris')
 
-    it('Should display title and description for each slide', () => {
-        cy.get('.swiper-slide').each(($slide, index) => {
-            cy.wrap($slide).within(() => {
-                cy.get('.slide-title').should('be.visible');
-                cy.get('.slide-description').should('be.visible');
-            });
-
-            if (index < Cypress.$('.swiper-slide').length - 1) {
-                cy.get('.swiper-button-next').click();
-            }
-        });
-    });
-});
-
-describe('Gallery Responsive Tests', () => {
-    const viewports = [
-        { device: 'macbook-15', width: 1440, height: 900 },
-        { device: 'ipad-2', width: 768, height: 1024 },
-        { device: 'iphone-x', width: 375, height: 812 }
-    ];
-
-    viewports.forEach((viewport) => {
-        it(`Should display correctly on ${viewport.device}`, () => {
-            cy.viewport(viewport.width, viewport.height);
-            cy.visit('http://localhost:3000');
-            cy.get('.gallery-container').should('be.visible');
-            cy.get('.swiper-button-next').should('be.visible').click();
-            cy.get('.swiper-button-prev').should('be.visible').click();
-        });
-    });
-});
-
-describe('Gallery Visibility Tests', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:3000');
-    });
-
-    it('Should display gallery elements correctly', () => {
-        cy.get('.gallery-container').should('be.visible');
-        cy.get('.swiper-slide').should('have.length.at.least', 3);
-        cy.get('.swiper-button-next').should('be.visible').and('not.be.disabled');
-        cy.get('.swiper-button-prev').should('be.visible').and('not.be.disabled');
     });
 });
